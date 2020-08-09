c65a320b82476c3a6a7c56bebe1b654d4dfcde01
Chitaarun <pratay.das08@gmail.com>
2020-08-09 12:13:42 -0400

    git sdet string conversion

---
+++ src/main/java/git/conversion.java
@@ -0,0 +1,19 @@
+package git;
+
+public class conversion {
+
+	public static void main(String[] args) {
+		
+	
+	String s = "arun kumar";
+	StringBuffer sb = new StringBuffer(s);
+	System.out.println(sb);
+	
+	StringBuffer sb1 = new StringBuffer("arunk");
+	
+	String ss = "arunk";
+	  System.out.println(sb1);  
+	  String s1=sb1.toString();
+	
+}
+}
